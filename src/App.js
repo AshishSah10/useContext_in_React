e9088393a083976d49c0fa2a30b7421574311fa2
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2021-05-21 11:52:42 +0530

    commit1 -implemented counter app using useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { createContext, useContext, useState } from "react";
 import "./styles.css";
 
+const CounterContext = createContext([0, () => {}]);
+
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <CounterContext.Provider value={[count, setCount]}>
+      <div className="App">
+        <Comp1/>
+      </div>
+    </CounterContext.Provider>
   );
+}
+
+const Comp1 = () => {
+  return(
+    <>
+      <div>Comp1</div>
+      <Comp2/>
+    </>
+  )
+}
+const Comp2 = () => {
+  return(
+    <>
+      <div>Comp2</div>
+      <Comp3/>
+    </>
+  )
+}
+const Comp3 = () => {
+  return(
+    <>
+      <div>Comp3</div>
+      <Comp4/>
+    </>
+  )
+}
+const Comp4 = () => {
+const [count, setCount] = useContext(CounterContext);
+  return(
+    <>
+      <div>Comp4</div>
+      <div>{count}</div>
+      <button onClick={() => setCount(count+1)}>increseCount</button>
+      <button onClick={() => setCount(count-1)}>decreaseCount</button>
+    </>
+  )
 }
